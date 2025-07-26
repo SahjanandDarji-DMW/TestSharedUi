436d5fbbfdf3f0c956210c8fb7757b1f825df8bd
SahjanandDarji-DMW <sahjanand.darji@digitalmetaworks.com>
2025-07-26 16:39:26 +0530

    update

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({
+  children,
+  variant = "primary",
+  size = "medium",
+  disabled = false,
+  onClick,
+  ...props
+}) => {
+  const className = `shared-btn shared-btn--${variant} shared-btn--${size} ${
+    disabled ? "shared-btn--disabled" : ""
+  }`;
+
+  return (
+    <button
+      className={className}
+      disabled={disabled}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
