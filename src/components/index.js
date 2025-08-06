cb30b16dfb2bfd47bb72bdb627fc81e5d3acaebe
SahjanandDarji-DMW <sahjanand.darji@digitalmetaworks.com>
2025-08-06 12:27:06 +0530

    export from index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import { CardShared } from "./cards/CardShared";
 import { ModalShared } from "./Modal/Modal";
 import { GlassCard } from "./cards/GlassCard";
 import { GlassButton } from "./Button/GlassButton";
+import { NavTabs } from "./NavTabs/index";
 
-export { ButtonSheard, CardShared, ModalShared, GlassCard, GlassButton };
+export {
+  ButtonSheard,
+  CardShared,
+  ModalShared,
+  GlassCard,
+  GlassButton,
+  NavTabs,
+};
